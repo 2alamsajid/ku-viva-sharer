80d537b284154bec652feeb68f4a4ee53e5d8954
2mdsajid <2mdsajid@gmail.com>
2025-05-07 12:40:32 +0545

    fuckkkkkkkk

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -7,6 +7,11 @@ export default defineConfig({
   schema: './lib/drizzle/schema.ts',
   out: './lib/drizzle/migrations.ts',
   dbCredentials:{
-    url: process.env.DATABASE_URL!
+    host: process.env.DATABASE_HOST!,
+    port: parseInt(process.env.DATABASE_PORT!),
+    user: process.env.DATABASE_USER!,
+    password: process.env.DATABASE_PASSWORD!,
+    database: process.env.DATABASE_NAME!,
+    ssl: { ca: process.env.DATABASE_CA! },
   },
 })
